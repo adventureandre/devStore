ba51b79d11cee9978a1204e0bec6a2cda2efc1c1
Andre luiz <adventureandre@hotmail.com>
2024-06-18 23:46:10 -0300

    iniciando header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,24 @@
+import Link from 'next/link'
+import { Search } from 'lucide-react'
+
 export function Header() {
-  return <div>Header</div>
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore
+        </Link>
+
+        <form className="flex w-[320px] items-center gap-5 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+          <Search className="w-5 h-5" />
+          <input
+            type="text "
+            placeholder="Buscar Produtos... "
+            className=" flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-50"
+          />
+        </form>
+      </div>
+      <div className="flex itens-center gap-4">aki</div>
+    </div>
+  )
 }
