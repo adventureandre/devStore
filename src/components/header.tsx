01a8be9d750f0864f62b58ae552d8b230c5656cc
Andre luiz <adventureandre@hotmail.com>
2024-06-19 21:31:09 -0300

    started header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import { Search } from 'lucide-react'
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
 
 export function Header() {
   return (
@@ -10,15 +11,37 @@ export function Header() {
         </Link>
 
         <form className="flex w-[320px] items-center gap-5 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="w-5 h-5" />
+          <Search
+            className="w-5 h-5 text-zinc-500
+          "
+          />
           <input
             type="text "
             placeholder="Buscar Produtos... "
-            className=" flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-50"
+            className=" flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
           />
         </form>
       </div>
-      <div className="flex itens-center gap-4">aki</div>
+      <div className="flex itens-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="h-4 w-4" />
+          <span className="text-sm">Cart(0)</span>
+        </div>
+
+        <div className="w-px h-4 bg-zinc-700" />
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Account</span>
+
+          <Image
+            src="https://github.com/adventureandre.png"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+            alt=""
+          />
+        </Link>
+      </div>
     </div>
   )
 }
