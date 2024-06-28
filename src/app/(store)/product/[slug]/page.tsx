424d4078122734c2624bd26123b04cc748c2b1fb
Andre luiz <adventureandre@hotmail.com>
2024-06-28 11:31:33 -0300

    Detalhe do produto

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -34,11 +34,39 @@ export default function ProductPage() {
           <span className="block font-semibold">Tamanho</span>
 
           <div className="flex gap-2">
-            <button type="button" className="flex h-9 w-14">
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
               P
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              M
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              G
+            </button>
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              GG
             </button>
           </div>
         </div>
+
+        <button
+          type="button"
+          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold"
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     </div>
   )
