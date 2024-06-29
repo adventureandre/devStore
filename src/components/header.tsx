487f6629bee1a673178a749706dc3f84a4beecf2
Andre luiz <adventureandre@hotmail.com>
2024-06-29 18:30:25 -0300

    feat: Formulário de busca

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
-import { Search } from 'lucide-react'
 import Image from 'next/image'
 import { CartWidget } from './cart-widget'
+import { SearchForm } from './search-form'
 
 export function Header() {
   return (
@@ -11,17 +11,7 @@ export function Header() {
           devstore
         </Link>
 
-        <form className="flex w-[320px] items-center gap-5 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search
-            className="w-5 h-5 text-zinc-500
-          "
-          />
-          <input
-            type="text "
-            placeholder="Buscar Produtos... "
-            className=" flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
       <div className="flex itens-center gap-4">
         <CartWidget />
