986d68865bd678378153cd2bbe8ae066c1ed3162
Andre luiz <adventureandre@hotmail.com>
2024-07-03 12:36:52 -0300

    feat: Testando a busca

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,7 +1,9 @@
 describe('template spec', () => {
-  it('should be able to navigate to the product page and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
+  it('should be able to navigate to the product page and add it to the cart', () => {
     cy.get('a[href^="/product"]').first().click()
     // cy.url().should('include', '/product')
     cy.location('pathname').should('include', '/product')
@@ -10,9 +12,8 @@ describe('template spec', () => {
 
     cy.contains('Cart(1)').should('exist')
   })
-  it('should not count duplicated products on cart', () => {
-    cy.visit('http://localhost:3000')
 
+  it('should not count duplicated products on cart', () => {
     cy.get('a[href^="/product"]').first().click()
     // cy.url().should('include', '/product')
     cy.location('pathname').should('include', '/product')
@@ -24,11 +25,10 @@ describe('template spec', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
     cy.get('input[name=q]').type('moletom').parent('form').submit()
 
     cy.get('a[href^="/product"]').first().click()
-    
+
     cy.location('pathname').should('include', '/product')
 
     cy.contains('Adicionar ao carrinho').click()
