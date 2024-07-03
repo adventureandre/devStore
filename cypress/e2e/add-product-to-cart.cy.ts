4b34985108bb7e249ea6b4675f99256c9d49b26d
Andre luiz <adventureandre@hotmail.com>
2024-07-03 11:51:06 -0300

    feat: test duplication product on cart

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -10,13 +10,14 @@ describe('template spec', () => {
 
     cy.contains('Cart(1)').should('exist')
   })
-  it('should be able to navigate to the product page and add it to the cart', () => {
+  it('should not count duplicated products on cart', () => {
     cy.visit('http://localhost:3000')
 
     cy.get('a[href^="/product"]').first().click()
     // cy.url().should('include', '/product')
     cy.location('pathname').should('include', '/product')
 
+    cy.contains('Adicionar ao carrinho').click()
     cy.contains('Adicionar ao carrinho').click()
 
     cy.contains('Cart(1)').should('exist')
